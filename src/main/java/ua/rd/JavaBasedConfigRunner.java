0dbf902c801887e0a6378fb1617b13833260d977
Vladyslav_Zhuchkov <Vladyslav_Zhuchkov@EPUAKYIW2774.kyiv.epam.com>
2017-09-29 17:24:14 +0300

    Add usage example to main.

--- src/main/java/ua/rd/JavaBasedConfigRunner.java
+++ src/main/java/ua/rd/JavaBasedConfigRunner.java
@@ -21,29 +21,39 @@ public class JavaBasedConfigRunner {
 		serviceContext.refresh();
 		UserService userService = serviceContext.getBean(UserService.class);
 		TweetService tweetService = serviceContext.getBean(TweetService.class);
+		//Registered user
 		User currentUser = userService.saveUser(userService.createNewUser("user1"));
 		
-		TimelineFilter config =serviceContext.getBean(TimelineFilter.class); 
-		
+		//User createed 2 tweets
 		tweetService.newTweet(currentUser);
 		tweetService.newTweet(currentUser);
-		System.out.println(tweetService.allUsersTweets(currentUser));
-		
+		System.out.println("User's timline:\n"+tweetService.userTimeline(currentUser)+"\n");
+		//user 2
 		User user2 = userService.saveUser(userService.createNewUser("user2"));
 		tweetService.newTweet(user2);
 		tweetService.newTweet(user2);
+		//user 3
 		User user3 = userService.saveUser(userService.createNewUser("user3"));
 		tweetService.newTweet(user3);
+		
 		Tweet targetTweet = tweetService.newTweet(user3);
+		targetTweet.setMaxTextLength(20L);
+		targetTweet.setTxt("targetTweet");
 		
+		//subscribe registered user on user with id 2
 		userService.createSubscription(currentUser, user2.getId());
-		userService.retweet(user2, targetTweet.getTweetId());
+		System.out.println("User's timline after subscription ou user 2:\n"+tweetService.userTimeline(currentUser)+"\n");
 		
-		System.out.println(tweetService.userTimeline(currentUser));
+		//User 2 retweeted user3's tweet, so user 1 can see targetTweet in his timeline;
+		userService.retweet(user2, targetTweet.getTweetId());
+		System.out.println("User's timline after user2 retweet targetTweet:\n"+tweetService.userTimeline(currentUser)+"\n");
+		
+		
+		//User1 sets likes to all tweets in his timeline
 		for(Tweet tweet :tweetService.userTimeline(currentUser)) {
 			tweetService.likeTweet(tweet.getTweetId());
 		}
 		
-		System.out.println(tweetService.userTimeline(currentUser));
+		System.out.println("User's timline after likes:\n"+tweetService.userTimeline(currentUser));
 	}
 }
